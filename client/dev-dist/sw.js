b3dd66c2ae23b266c842a515c6a47d33fc35c7f4
Awanish Verma <a2023cse9230@imsec.ac.in>
2026-07-01 19:56:28 +0530

    forget pasword issue handle

--- client/dev-dist/sw.js
+++ client/dev-dist/sw.js
@@ -81,7 +81,7 @@ define(['./workbox-7e5eb42b'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.fladvu30e18"
+    "revision": "0.9s4m9p2b1io"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
